dc561e19e1bd709ac5871f3ca359c12c8f62b576
Braeden Vandenbelt <braedenvandenbelt@Braedens-MacBook-Air.local>
2022-07-20 11:35:27 -0400

    Updated to 3.4.5

---
+++ PyPoll.py
@@ -0,0 +1,60 @@
+#Data we need to retrieve.
+#1. total number of votes cast
+#2. A complete list of candidates who received votes
+#3. The percentage of votes each candidate won
+#4. The total number of votes each candidate won
+#5. The winner of the election based on popular vote
+
+# Import the datetime class from the datetime module.
+import datetime as dt
+# Use the now() attribute on the datetime class to get the present time.
+now = dt.datetime.now()
+# Print the present time.
+print("The time right now is ", now)
+
+# # Assign a variable for the file to load and the path.
+# file_to_load = 'Resources_Mod_3/election_results.csv'
+
+# # Open the election results and read the file,
+# with open(file_to_load) as election_data:
+# # To do: perform analysis.
+#      print(election_data)
+# # Close the file.
+# election_data.close()
+
+import csv
+import os
+# Assign a variable for the file to load and the path.
+file_to_load = os.path.join("Resources_Mod_3", "election_results.csv")
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+
+    # Read the file object with the reader function.
+    file_reader = csv.reader(election_data)
+
+# Print the header row.
+    headers = next(file_reader)
+    print(headers)
+
+# # Create a filename variable to a direct or indirect path to the file.
+# file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# # Use the open statement to open the file as a text file.
+# outfile = open(file_to_save, "w")
+# # Write some data to the file.
+# outfile.write("Hello World Thanks for all your help so far")
+
+# # Close the file
+# outfile.close()
+
+
+# # Create a filename variable to a direct or indirect path to the file.
+# file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# # Using the with statement open the file as a text file.
+# with open(file_to_save, "w") as txt_file:
+
+#    # Write three counties to the file.
+#      txt_file.write("Arapahoe\nDenver\nJefferson")
+
+
